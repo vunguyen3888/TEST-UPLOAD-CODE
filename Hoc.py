5a07b1354e1c1deb4ec771f7c36e39b5d16280a8
vu nguyen <vudesign3888@gmail.com>
2021-11-20 14:18:59 +0700

    first

---
+++ Hoc.py
@@ -0,0 +1,17 @@
+'''
+n = int(input("Nhập số n:"))
+tong = 0
+i = 1
+
+while i<=n:
+    tong = tong + i
+    i = i+1
+    print("Tong cua so tu 1 den",n,"la",tong)
+
+
+'''
+
+a = 'NguyenVu'
+b = len(a)
+c = len(a[:3])
+print(b+c)
---
+++ Hoc_py.py
@@ -0,0 +1,11 @@
+a = "Nguyen Vu"
+c = a.replace(' ','')
+
+b = list(c)
+
+e = "*****"
+f = e.join(b)
+print(f)
+
+
+
